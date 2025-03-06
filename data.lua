c47c9e27e0905b4aefb088ac81c1ba8ff3355f98
Alexander Sveshnikov <diamondliquid@gmail.com>
2025-03-06 16:37:21 +0300

    repo init

---
+++ data.lua
@@ -0,0 +1,10 @@
+-- Change default_tileset
+data.raw["gui-style"].default["default_tileset"] = "__TechTreeColors__/graphics/gui-new.png"
+
+
+
+
+-- log all values into %APPDATA%\Factorio\factorio-current.log for debug
+-- for k, v in pairs(data.raw["gui-style"].default) do
+--     log("\n[" .. k .. "]\n" .. serpent.block(v) .. "\n")
+-- end
